46a2e00c63eb8c4029b12afb5dc77c9b9d8d0ce7
Kenni Bawden <kenni.bawden@gmail.com>
2017-07-14 17:16:01 +1000

    Styled map markers

--- src/Maps/Internal/Marker.elm
+++ src/Maps/Internal/Marker.elm
@@ -18,17 +18,23 @@ view map marker =
     DefaultMarker latLng ->
       let
         offset = Screen.offsetFromLatLng map latLng
+        width = 18
+        height = 36
       in
         Html.span
           [ Attr.style
             [ ("position", "absolute")
-            , ("left", toString offset.x ++ "px")
-            , ("top", toString offset.y ++ "px")
+            , ("left", toString (offset.x  - width/2) ++ "px")
+            , ("top", toString (offset.y - height) ++ "px")
             , ("display", "inline-block")
-            , ("width", "10px")
-            , ("height", "16px")
-            , ("background-color", "red")
+            , ("width", toString width ++ "px")
+            , ("height", toString height ++ "px")
+            , ("background-image", "url(\"data:image/svg+xml;utf-8,"++markerSvg++"\")")
+            , ("background-size", "100% auto")
             ]
           ]
           [
           ]
+
+markerSvg =
+  """<svg xmlns='http://www.w3.org/2000/svg' x='0px' y='0px' width='12' height='24' fill='#cc6666' stroke='#000000' version='1.1' viewBox='0 0 12 24'><path d='M 2,12 A 5.5,5.5 0 1,1 10,12 C 8,14 6,22 6,24 C 6,22 4,14 2,12 z  M 4,8.5 A 2,2 0 1,0 8,8.5 A 2,2 0 0,0 4,8.5 z' fill-rule='evenodd'/></svg>"""
