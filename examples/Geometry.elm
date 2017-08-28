909d83c42a780e1896f6235f3ff3a148cec75fa0
Kenni Bawden <kenni.bawden@gmail.com>
2017-08-28 20:25:22 +1000

    Simplified geometry example

--- examples/Geometry.elm
+++ examples/Geometry.elm
@@ -1,8 +1,6 @@
 module Polygons exposing (..)
 
 import Html exposing (program)
-import Html.Attributes as Attr
-import Html.Events exposing (onClick)
 
 import Http
 import Json.Decode as Json
@@ -15,16 +13,11 @@ import Maps.Geometry as Geometry exposing (Geometry)
 
 type Msg
   = MapsMsg (Maps.Msg Msg)
-  | Select Country
-  | LoadCountries (Result Http.Error GeoJson)
+  | LoadGeometry (Result Http.Error GeoJson)
 
 type alias Model =
   { map : Maps.Model Msg
-  , countries : List Country
-  , selected : Maybe Country
   }
-
-type alias Country = (String, GeoJson.Geometry)
 
 main = program
   { init = init
@@ -38,15 +31,13 @@ init =
       Maps.defaultModel
       |> Maps.updateMap (Map.setZoom 1)
       |> Maps.updateMap (Map.moveTo <| Maps.Geo.latLng 0 0)
-    , countries = []
-    , selected = Nothing
     }
-  , Http.send LoadCountries getCountries
+  , Http.send LoadGeometry getGeometry
   )
 
-getCountries : Http.Request GeoJson
-getCountries = 
-  Http.get "./countries.geo.json" GeoJson.decoder
+getGeometry : Http.Request GeoJson
+getGeometry =
+  Http.get "./example.geo.json" GeoJson.decoder
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -60,46 +51,23 @@ update msg model =
         |> Maps.update msg
         |> Tuple.mapFirst (\map -> { model | map = map }) 
         |> Tuple.mapSecond (Cmd.map MapsMsg)
-    Select country ->
-      ({ model | selected = Just country }, Cmd.none)
-    LoadCountries result ->
-      let
-        countries = resultToCountries result
-      in
-        ( { model
-          | map = model.map |> Maps.updateGeometry (\geometry -> countries |> List.map Tuple.second |> List.map Geometry.create)
-          , countries = countries
-          }
-       , Cmd.none
-       )
+    LoadGeometry result ->
+      ( { model
+        | map = model.map |> Maps.updateGeometry (\_ -> resultToGeometry result |> List.map Geometry.create)
+        }
+     , Cmd.none
+     )
 
-resultToCountries : Result Http.Error GeoJson -> List Country
-resultToCountries result =
-  let
-    featureToCountry feature =
-      Maybe.map2 (,)
-        (property "name" feature |> Result.toMaybe)
-        (feature.geometry)
-    property propertyName feature =
-      Json.decodeValue
-        (Json.field propertyName Json.string)
-        feature.properties
-  in
-    case result of
-      (Result.Ok (GeoJson.FeatureCollection features, _)) ->
-        features
-        |> List.filterMap featureToCountry
-      _ -> []
+resultToGeometry : Result Http.Error GeoJson -> List GeoJson.Geometry
+resultToGeometry result =
+  case result of
+    (Result.Ok (GeoJson.FeatureCollection features, _)) ->
+      features
+      |> List.filterMap .geometry
+    _ -> []
 
 view model =
   Html.div
     []
     [ Maps.view model.map |> Maps.mapView MapsMsg
-    , case model.selected of
-      Just (country, geometry) ->
-        Html.div
-          []
-          [ Html.text country ]
-      Nothing ->
-        Html.p [] [ Html.text "Mouse over a country" ]
     ]
